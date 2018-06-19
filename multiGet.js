a973bed1d8906e888e7c876ece4d2915250871f2
Niicck <nickivons@yahoo.com>
2018-06-19 14:54:09 -0600

    complete parallel implementation

--- multiGet.js
+++ multiGet.js
@@ -8,20 +8,11 @@ const fse = require('fs-extra')
 const path = require('path')
 const ProgressBar = require('progress');
 
-// Function to retrieve range of bytes from target file
-const getChunk = (uri, startRange, endRange) => {
-  return request({
-    uri,
-    method: "GET",
-    encoding: null, // required in order to process binary data
-    headers: {
-      Range: `bytes=${startRange}-${endRange}`
-    }
-  })
-}
-
 /**
+Downloads part of a file from a web server, in chunks.
+Defaults to 4 chunks of 1MB each.
 Usage: node ./multiGet.js [OPTIONS] url
+Flags:
   -o string
     	Write output to <file> instead of default
   -parallel
@@ -32,6 +23,37 @@ examples:
   node multiGet.js -o small-file http://31279842.bwtest-aws.pravala.com/384MB.jar
 **/
 const multiGet = () => {
+  /******
+  Downloads a single chunk from target uri
+  @param chunkSize: size of chunk
+  @param chunkNumber: index of current chunk
+  ******/
+  const getChunk = (chunkSize, chunkNumber) => {
+    const startRange = chunkNumber * chunkSize
+    const endRange = (chunkNumber + 1) * chunkSize - 1
+    return request({
+      uri,
+      method: "GET",
+      encoding: null, // required in order to process binary data
+      headers: {
+        Range: `bytes=${startRange}-${endRange}`
+      }
+    })
+  }
+  /******
+  Writes chunk to target file
+  @param file: writeStream to target file
+  @param chunk: chunk being written to file
+  @param chunkNumber: index of current chunk
+  @param bar: progress bar instance
+  ******/
+  const writeChunk = (file, chunk, chunkNumber, bar) => {
+    file.write(chunk)
+    bar.tick({
+      doneFlag: (chunkNumber === chunkCount-1 ? "done" : "")
+    })
+  }
+
 
   /******
   Initialize Variables
@@ -54,7 +76,7 @@ const multiGet = () => {
   // TODO: make flags to alter these defaults
   const lengthLowerBound = 4000000
   let chunkSize = 1000000
-  let totalSize = 4000000
+  let totalDownloadSize = 4000000
   let chunkCount = 4
 
   /******
@@ -97,36 +119,27 @@ const multiGet = () => {
 
     if (!parallel) {
       /******
-      Retrieve file in chunks serially
+      Download file in chunks serially
       ******/
-      return Promise.mapSeries(_.range(chunkCount), (i) => {
-        const startRange = i * chunkSize
-        const endRange = (i + 1) * chunkSize - 1
-        return request({
-          uri,
-          method: "GET",
-          encoding: null, // required in order to process binary data
-          headers: {
-            Range: `bytes=${startRange}-${endRange}`
-          }
-        })
-        .then((chunk) => {
-          file.write(chunk)
-          bar.tick({
-            doneFlag: (i === chunkCount-1 ? "done" : "")
-          })
-        })
+      return Promise.mapSeries(_.range(chunkCount), (chunkNumber) => {
+        return getChunk(chunkSize, chunkNumber)
+        .then((chunk) => writeChunk(file, chunk, chunkNumber, bar))
       })
-      .then(() => {
-        file.end()
-      })
+      .then(() => file.end())
     } else {
       /******
-      TODO: Retrieve file in chunks in parallel
+      Retrieve file in chunks in parallel
       ******/
-      return Promise.resolve()
+      return Promise.map(_.range(chunkCount), (chunkNumber) => {
+        return getChunk(chunkSize, chunkNumber)
+      }, {concurrency: 2}) // Could change concurrency value
+      .then((chunks) => {
+        return _.each(chunks, (chunk, chunkNumber) => {
+          writeChunk(file, chunk, chunkNumber, bar)
+        })
+      })
+      .then(() => file.end())
     }
-
   })
   .catch((err) => {
     console.log(`[ERROR] ${err.message}`)
