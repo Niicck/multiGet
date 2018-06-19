70cd9b08d791b9e119eb888fa310ca460ebb6ae2
Niicck <nickivons@yahoo.com>
2018-06-19 10:15:48 -0600

    begin serial implementation

--- multiGet.js
+++ multiGet.js
@@ -1,36 +1,81 @@
-'use strict';
+'use strict'
 
-const http = require('http');
-const request = require('request-promise');
+const request = require('request-promise')
+const Promise = require('bluebird')
+const _ = require('lodash')
+const fs = require('fs')
+const path = require('path')
 
 const lengthLowerBound = 4000000
-let chunkSize = 1000000
+let chunkSize = 1000
+let totalSize = 4000000
+let chunkCount = 4
+let parallel = false
 
-// Test Values
-const testUrl = 'http://31279842.bwtest-aws.pravala.com/384MB.jar'
-
-
+// Function to retrieve range of bytes from target file
+const getChunk = (uri, startRange, endRange) => {
+  return request({
+    uri,
+    method: "GET",
+    Range: `bytes=${startRange}-${endRange}`
+  })
+  .then((res) => {
+    console.log("We got a chunk response")
+    return res.body
+  })
+}
 
 const test = (uri) => {
+  const destination = __dirname + '/' + path.basename(uri)
+  const file = fs.createWriteStream(destination) //TODO: check for existence [ERROR] File '384MB.jar' exists
 
-  const options = {
+  // Determine length of target file
+  return request({
     uri,
     method: "HEAD"
-  }
-
-  request(options)
+  })
   .then((res) => {
     console.log(res)
     const length = res['content-length']
     if (length < lengthLowerBound) {
+      console.log(`length is ${length}`)
       // TODO: Adjust chunkSize accordingly
     }
-
     console.log(`how long? ${length}`)
+    return
   })
-  .then()
+  .then(() => {
+    if (!parallel) {
+      // Retrieve file in chunks serially
+      return Promise.mapSeries(_.range(chunkCount), (i) => {
+        const startRange = i * chunkSize
+        const endRange = (i + 1) * chunkSize - 1
+        console.log("About to get a chunk")
+        return getChunk(uri, startRange, endRange)
+        .then((chunk) => {
+          console.log("What is chunk?")
+          console.log(chunk)
+          file.write(chunk)
+          console.log(`Wrote ${i+1} chunk!`)
+          return Promise.resolve()
+        })
+      })
+      .then(() => file.end())
+    } else {
+      // TODO: Retrieve file in chunks in parallel
+      return Promise.resolve()
+    }
 
-
+  })
+  .error((err) => {
+    console.log("Problems!")
+    console.log(err)
+  })
+  .finally(() => {
+    file.end()
+    return Promise.resolve()
+  })
 }
 
+const testUrl = 'http://31279842.bwtest-aws.pravala.com/384MB.jar'
 test(testUrl)
---
+++ test.js
@@ -0,0 +1,19 @@
+'use strict'
+
+// const Promise = require('bluebird')
+//
+// const derp = () => {
+//   let file = 55
+//   return Promise.resolve()
+//   .then(()=>file)
+//   .then((f)=>{
+//     console.log(`file is ${f}`)
+//   })
+// }
+//
+// derp()
+
+
+const path = require('path')
+const filename = path.basename('http://31279842.bwtest-aws.pravala.com/38434125MB.jar')
+console.log(filename)
