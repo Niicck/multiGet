6ac0f3f6ef7a1e23161fc47cc1638c1c886b971b
Niicck <nickivons@yahoo.com>
2018-06-19 23:17:43 -0600

    create README

---
+++ README.md
@@ -0,0 +1,32 @@
+##multiGet
+
+Downloads part of a file from a web server, in chunks.<br>
+Defaults to 4 chunks of 1MB each.<br>
+
+**Installation**<br>
+run `npm install` to install all required npm packages.<br>
+Tested on node v10.4.1 on macOS 10.11.6<br>
+Mocha test suite can be run with `mocha test`
+
+**Usage**<br>
+node ./multiGet.js [OPTIONS] url<br>
+Flags:
+  + -o string<br>
+    	Write output to <file> instead of default
+  + -parallel<br>
+    	Download chunks in parallel instead of serially
+  + -chunk-count integer<br>
+      Specify number of chunks. Default is 4.
+  + -chunk-size integer<br>
+      Specify the size of each chunk. Default is 1MB.<br>
+      If a chunk-count is already specified, then the -chunk-size flag will be ignored.<br>
+      Note: this value will be recalculated if the actual target file size is less than the specified total size.
+  + -total-size integer<br>
+      Specify the total download size. Default is 4MB.
+
+**Examples**
+  + `node index.js http://31279842.bwtest-aws.pravala.com/384MB.jar`
+  + `node index.js -o my_file http://31279842.bwtest-aws.pravala.com/384MB.jar`
+  + `node index.js -o my_file2 -chunk-size 512638 http://31279842.bwtest-aws.pravala.com/384MB.jar`
+  + `node index.js -o my_file3 -chunk-count 5 http://31279842.bwtest-aws.pravala.com/384MB.jar`
+  + `node index.js -o my_file4 -chunk-size 712638 -total-size 5000000 -parallel http://31279842.bwtest-aws.pravala.com/384MB.jar`
--- test/test.js
+++ test/test.js
@@ -33,6 +33,7 @@ describe('multiGet', function(){
 
   it('should download file with default settings', function() {
     return multiGet(uri, testFilename)
+    .then(() => Promise.delay(1000)) // Slight delay between writing and Buffer comparison. TODO: discover cause
     .then(() => {
       const testFile = fs.readFileSync(testFilepath)
       assert.equal(Buffer.compare(correctFile, testFile), 0)
@@ -42,7 +43,7 @@ describe('multiGet', function(){
   it('should download file with default settings in parallel', function() {
     // node index.js -o test-file http://31279842.bwtest-aws.pravala.com/384MB.jar
     return multiGet(uri, testFilename, true)
-    .then(() => Promise.delay(1000)) // Parallel Implementation has slight delay
+    .then(() => Promise.delay(1000))
     .then(() => {
       const testFile = fs.readFileSync(testFilepath)
       assert.equal(Buffer.compare(correctFile, testFile), 0)
@@ -52,6 +53,7 @@ describe('multiGet', function(){
   it('should download file with non-default chunk-size', function() {
     // node index.js -o test-file -chunk-size 512638 http://31279842.bwtest-aws.pravala.com/384MB.jar
     return multiGet(uri, testFilename, null, null, 512638)
+    .then(() => Promise.delay(1000))
     .then(() => {
       const testFile = fs.readFileSync(testFilepath)
       assert.equal(Buffer.compare(correctFile, testFile), 0)
@@ -61,7 +63,7 @@ describe('multiGet', function(){
   it('should download file with non-default chunk-size in parallel', function() {
     // node index.js -o test-file -chunk-size 512638 -parallel http://31279842.bwtest-aws.pravala.com/384MB.jar
     return multiGet(uri, testFilename, true, null, 512638)
-    .then(() => Promise.delay(1000)) // Parallel Implementation has slight delay
+    .then(() => Promise.delay(1000))
     .then(() => {
       const testFile = fs.readFileSync(testFilepath)
       assert.equal(Buffer.compare(correctFile, testFile), 0)
@@ -71,6 +73,7 @@ describe('multiGet', function(){
   it('should download file with non-default chunk-count', function() {
     // node index.js -o test-file -chunk-count 5 http://31279842.bwtest-aws.pravala.com/384MB.jar
     return multiGet(uri, testFilename, null, 5)
+    .then(() => Promise.delay(1000))
     .then(() => {
       const testFile = fs.readFileSync(testFilepath)
       assert.equal(Buffer.compare(correctFile, testFile), 0)
@@ -80,7 +83,7 @@ describe('multiGet', function(){
   it('should download file with non-default chunk-count in parallel', function() {
     // node index.js -o test-file -chunk-count 5 -parallel http://31279842.bwtest-aws.pravala.com/384MB.jar
     return multiGet(uri, testFilename, true, 5)
-    .then(() => Promise.delay(1000)) // Parallel Implementation has slight delay
+    .then(() => Promise.delay(1000))
     .then(() => {
       const testFile = fs.readFileSync(testFilepath)
       assert.equal(Buffer.compare(correctFile, testFile), 0)
